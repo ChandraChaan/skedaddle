4754c146ef0633754c33efec45ccc7ff92d17b32
swarnaawasthy <swarnaawasthy@15gmail.com>
2021-07-20 22:54:29 +0530

    forget password UI Changes

--- lib/Presantation/forgetPassword.dart
+++ lib/Presantation/forgetPassword.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:skedaddle/Common/Button.dart';
+import 'package:skedaddle/Common/textField.dart';
 
 class forgetPassword extends StatefulWidget {
   @override
@@ -7,45 +8,78 @@ class forgetPassword extends StatefulWidget {
 }
 
 class _forgetPasswordState extends State<forgetPassword> {
+
+  final emailControl = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text("Forget Password",),
-            ),
-            Text("Enter your email address below to reset password.",),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: TextFormField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Email',
 
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 32.0),
+          child: Column(
+
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Align(
+                alignment: Alignment.topLeft,
+                child: Text("Forget Password",
+                  style: TextStyle(color: Colors.black,fontSize: 30.0,fontWeight: FontWeight.bold,fontFamily: 'san-serif'),),
+              ),
+              SizedBox(
+                height: 4.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 120.0),
+                child: Text("Enter your email address below to reset password.",style: TextStyle(color: Colors.black,fontSize: 16.0,fontWeight: FontWeight.normal,fontFamily: 'san-serif'),),
+              ),
+              SizedBox(
+                height: 66.0,
+              ),
+              /*Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Email',
+
+                  ),
                 ),
               ),
-            ),
+*/
+              UiFormText(
+                  hint: 'Email',
+                  label: 'email',
+                  controllercc: emailControl,
+                  normalFeild: true,
+                  readonlyye: false),
+              SizedBox(
+                height: 20.0,
+              ),
+              /*Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Container(
+                  height: 50,
+                  width: MediaQuery.of(context).size.width ,
+                  child: themeButton(
+                    name:  'Reset Password',
+                    onClick: () {
 
 
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Container(
-                height: 50,
-                width: MediaQuery.of(context).size.width ,
+                    },
+                  ),
+                ),
+              ),*/
+              Container(
+                height: 56,
+                width: MediaQuery.of(context).size.width,
                 child: themeButton(
-                  name:  'Reset Password',
-                  onClick: () {
-
-
-                  },
+                  name: 'Reset Password',
+                  onClick: () {},
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       )
     );
